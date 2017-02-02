f91282e9b2095a9effc47ddf9ce0ceca929062c8
pdxdive <ddcarr@gmail.com>
2017-02-01 23:40:15 -0800

    mo tagging

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # Push only if it's not a pull request
 if [ "$TRAVIS_BRANCH" == "master" ]; then
+    docker tag pdxdiver/django-web:latest pdxdiver/django-web:"$COMMIT"
     docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
     docker push pdxdiver/django-web &&
     ecs-cli compose --project-name ecs-hacko --file ecs-deploy.yml service up;
