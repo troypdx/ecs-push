34ac582f7d9644adc411c6d630a160ca1e513966
pdxdive <ddcarr@gmail.com>
2017-02-08 10:56:17 -0800

    scriptify compose stuff

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -5,5 +5,5 @@ source ./bin/env.sh
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
     docker push "$DOCKER_REPO"/"$DOCKER_WEB_IMAGE":latest
-    ecs-cli compose --project-name "$ECS_PROJECT --file ecs-deploy.yml service up;
+    ecs-cli compose --project-name "$ECS_PROJECT" --file ecs-deploy.yml service up;
 fi
