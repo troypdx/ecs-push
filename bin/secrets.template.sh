3f29e19aef91458497155ab5c8c79f66907f91d6
pdxdive <ddcarr@gmail.com>
2017-02-09 12:04:02 -0800

    for the team

--- bin/secrets.template.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# Setup Project Specfics - Secrets
-# Do not upload to github.  Make sure secrets.sh is in .gitignore
-export AWS_ACCESS_KEY=<YOUR STUFF HERE>
-export AWS_SECRET_KEY=<YOUR STUFF HERE>
-export DOCKER_USERNAME=<YOUR STUFF HERE>
-export DOCKER_PASSWORD=<YOUR STUFF HERE>
